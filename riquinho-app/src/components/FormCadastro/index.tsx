c975b059771c8aa89e5853db17e436194c6c7543
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-02-14 13:27:44 -0300

    feat(front) implementa uso de rem e min height e width nos svgs de login e cadastro

--- riquinho-app/src/components/FormCadastro/index.tsx
+++ riquinho-app/src/components/FormCadastro/index.tsx
@@ -1,43 +1,48 @@
-import styles from "./styles.module.scss";
-import { Link } from 'react-router-dom'
+import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
 
 export function FormCadastro() {
   return (
     <form className={styles.cadastro} action="">
-      <label htmlFor="email">Nome</label>
-      <input
-        type="text"
-        name="email"
-        id="email"
-        placeholder="seu nome"
-        autoComplete="off"
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        id="email"
-        placeholder="seuemail@email.com"
-        autoComplete="off"
-      />
-      <label htmlFor="senha">Senha</label>
-      <input
-        type="password"
-        name="senha"
-        id="senha"
-        placeholder="**************"
-      />
-      <label htmlFor="senha">Confirmar Senha</label>
-      <input
-        type="password"
-        name="senha"
-        id="senha"
-        placeholder="**************"
-      />
-      <button type="submit">Cadastrar</button>
-      <p>
-        Já tem uma conta? <Link to='/login'>entre agora</Link>
-      </p>
+      <div className={styles.inputs}>
+        <label htmlFor="email">Nome</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="seu nome"
+          autoComplete="off"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="seuemail@email.com"
+          autoComplete="off"
+        />
+        <label htmlFor="senha">Senha</label>
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          placeholder="**************"
+        />
+        <label htmlFor="senha">Confirmar Senha</label>
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          placeholder="**************"
+        />
+      </div>
+
+      <div className={styles.submit}>
+        <button type="submit">Cadastrar</button>
+        <p>
+          Já tem uma conta? <Link to="/login">entre agora</Link>
+        </p>
+      </div>
     </form>
   );
 }
--- riquinho-app/src/components/FormLogin/index.tsx
+++ riquinho-app/src/components/FormLogin/index.tsx
@@ -1,15 +1,33 @@
-import styles from './styles.module.scss'
-import { Link } from 'react-router-dom'
+import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
 
 export function FormLogin() {
-  return(
-    <form className={styles.login} action="">
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" placeholder="seuemail@email.com" autoComplete='off' />
-      <label htmlFor="senha">Senha</label>
-      <input type="password" name="senha" id="senha" placeholder="**************"/>
-      <button type="submit">Entrar</button>
-      <p>Ainda não tem uma conta? <Link to="/cadastro">cadastre-se</Link></p>
+  return (
+    <form className={styles.login} action="#">
+      <div className={styles.inputs}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="seuemail@email.com"
+          autoComplete="off"
+        />
+        <label htmlFor="senha">Senha</label>
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          placeholder="**************"
+        />
+      </div>
+
+      <div className={styles.submit}>
+        <button type="submit">Entrar</button>
+        <p>
+          Ainda não tem uma conta? <Link to="/cadastro">cadastre-se</Link>
+        </p>
+      </div>
     </form>
-  )
+  );
 }
