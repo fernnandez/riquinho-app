d9620e9ffe97c0ab44ee106154df65d1b7b3f9f1
Angelo Fernandes <afbo2003@gmail.com>
2022-02-14 22:10:47 -0300

    feat(front) implementa reactQuery para buscar as transacoes e continua implementando tela de transacoes

--- riquinho-app/src/components/ListTransacoes/index.tsx
+++ riquinho-app/src/components/ListTransacoes/index.tsx
@@ -1,37 +1,67 @@
-import styles from './styles.module.scss';
-import { MdOutlineFastfood } from 'react-icons/md';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { MdDeleteOutline } from 'react-icons/md';
+import { useQuery } from "react-query";
+import axios from "axios";
+
+import styles from "./styles.module.scss";
+import { MdOutlineFastfood } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdDeleteOutline } from "react-icons/md";
+
+import notFoundILustration from "../../assets/not-found.svg";
+
+type Transacao = {
+  id: string;
+  titulo: string;
+  descricao: string;
+  data: Date;
+  tipo: string;
+  categoria: string | null;
+  status: string;
+  valor: number;
+};
 
 export function ListTransacoes() {
+  const { data: transacoes, isFetching } = useQuery<Transacao[]>(
+    "transacoes",
+    async () => {
+      const response = await axios.get("http://localhost:3002/transacao");
+      return response.data;
+    }
+  );
+
   return (
     <div className={styles.listBg}>
-      <ul className={styles.list}>
-        <li className={styles.listRow}>
-          <div className={styles.info}>
-            <MdOutlineFastfood size={32} />
-            <p>Compras do mês</p>
-            <p>R$ 950,00</p>
-            <p>06/02/2022</p>
-          </div>
-          <div className={styles.actions}>
-            <AiOutlineEdit size={32} />
-            <MdDeleteOutline size={32} />
-          </div>
-        </li>
-        <li className={styles.listRow}>
-          <div className={styles.info}>
-            <MdOutlineFastfood size={32} />
-            <p>Compras do mês</p>
-            <p>R$ 950,00</p>
-            <p>06/02/2022</p>
-          </div>
-          <div className={styles.actions}>
-            <AiOutlineEdit size={32} />
-            <MdDeleteOutline size={32} />
-          </div>
-        </li>
-      </ul>
+      {isFetching || !transacoes ? (
+        <p>Carregando</p>
+      ) : (
+        <ul className={styles.list}>
+          {transacoes?.map((transacao) => {
+            return (
+              <li className={styles.listRow}>
+                <div className={styles.info}>
+                  <MdOutlineFastfood size={32} />
+                  <p>{transacao.titulo}</p>
+                  <p>R$ {transacao.valor}</p>
+                  <p>{transacao.data}</p>
+                </div>
+                <div className={styles.actions}>
+                  <AiOutlineEdit size={32} />
+                  <MdDeleteOutline size={32} />
+                </div>
+              </li>
+            );
+          })}
+          {transacoes.length == 0 ? (
+            <>
+              <img
+                className={styles.notFound}
+                src={notFoundILustration}
+                alt="not found ilustration"
+              />
+              <h2>Nenhuma transação encontada</h2>
+            </>
+          ) : null}
+        </ul>
+      )}
     </div>
   );
 }
--- riquinho-app/src/main.tsx
+++ riquinho-app/src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./services/queryClient";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
