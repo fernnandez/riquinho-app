694592158ce2230fb9ced395c246da3b29a14b00
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-06-24 09:48:56 -0300

    chore(*) remove antigo projeto de front

--- riquinho-app/src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const AuthContext = createContext({
-  token: '',
-  setToken: (token: string) => {},
-  remove: () => {},
-});
-
-export default AuthContext;
