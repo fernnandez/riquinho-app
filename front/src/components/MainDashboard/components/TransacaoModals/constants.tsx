d7077682f9b47ed1e841754d78a54131b5737001
Angelo Fernandes Oliveira <afbo2003@gmail.com>
2022-11-08 09:14:33 -0300

    feat(front) inclui novos icones na listagem de opcoes

--- front/src/components/MainDashboard/components/TransacaoModals/constants.tsx
+++ front/src/components/MainDashboard/components/TransacaoModals/constants.tsx
@@ -1,14 +1,17 @@
-import { MdAttachMoney } from 'react-icons/md';
+import { MdAttachMoney, MdDirectionsBike } from 'react-icons/md';
 import { TbCashBanknoteOff } from 'react-icons/tb';
 
-import { BiBuildingHouse, BiCategory } from 'react-icons/bi';
+import { BiBuildingHouse, BiCategory, BiHome } from 'react-icons/bi';
 import { BsStars } from 'react-icons/bs';
 import { GiMoneyStack } from 'react-icons/gi';
-import { IoFastFoodSharp } from 'react-icons/io5';
+import { IoFastFoodSharp, IoGameController } from 'react-icons/io5';
+import { FaGamepad } from 'react-icons/fa';
 
 import { ThemeIcon } from '@mantine/core';
 import { CategoriaResponse } from '../../../../services/categoria';
 import { ReactNode } from 'react';
+import { AiFillCar } from 'react-icons/ai';
+import { HiDesktopComputer } from 'react-icons/hi';
 
 export enum CategoriaEnum {
   ALIMENTACAO = 'ALIMENTACAO',
@@ -90,11 +93,17 @@ export function getSimpleIcon(
   sizeIcon = 45
 ): ReactNode {
   return {
-    ALIMENTACAO: <IoFastFoodSharp size={sizeIcon} color={color} />,
-    MORADIA: <BiBuildingHouse size={sizeIcon} color={color} />,
-    PAGAMENTO: <GiMoneyStack size={sizeIcon} color={color} />,
-    OUTROS: <BsStars size={sizeIcon} color={color} />,
-    CUSTOM: <BiCategory size={sizeIcon} color={color} />,
+    ['ALIMENTACAO']: <IoFastFoodSharp size={sizeIcon} color={color} />,
+    ['MORADIA']: <BiBuildingHouse size={sizeIcon} color={color} />,
+    ['PAGAMENTO']: <GiMoneyStack size={sizeIcon} color={color} />,
+    ['OUTROS']: <BsStars size={sizeIcon} color={color} />,
+    ['CUSTOM']: <BiCategory size={sizeIcon} color={color} />,
+    ['VIDEO-GAME']: <FaGamepad size={sizeIcon} color={color} />,
+    ['VIDEO-GAME2']: <IoGameController size={sizeIcon} color={color} />,
+    ['BIKE']: <MdDirectionsBike size={sizeIcon} color={color} />,
+    ['CARRO']: <AiFillCar size={sizeIcon} color={color} />,
+    ['CASA']: <BiHome size={sizeIcon} color={color} />,
+    ['COMPUTADOR']: <HiDesktopComputer size={sizeIcon} color={color} />,
   }[icon];
 }
 
@@ -118,5 +127,40 @@ export const iconList = [
     value: 'OUTROS',
     label: 'Outros',
     icon: getSimpleIcon('OUTROS', 'black'),
+  },
+  {
+    value: 'VIDEO-GAME',
+    label: 'Video Game',
+    icon: getSimpleIcon('VIDEO-GAME', 'black'),
+  },
+  {
+    value: 'VIDEO-GAME2',
+    label: 'Video Game',
+    icon: getSimpleIcon('VIDEO-GAME2', 'black'),
+  },
+  {
+    value: 'BIKE',
+    label: 'Bike',
+    icon: getSimpleIcon('BIKE', 'black'),
+  },
+  {
+    value: 'CARRO',
+    label: 'Carro',
+    icon: getSimpleIcon('CARRO', 'black'),
+  },
+  {
+    value: 'CASA',
+    label: 'Casa',
+    icon: getSimpleIcon('CASA', 'black'),
+  },
+  {
+    value: 'COMPUTADOR',
+    label: 'Computador',
+    icon: getSimpleIcon('COMPUTADOR', 'black'),
+  },
+  {
+    value: 'CUSTOM',
+    label: 'Custom',
+    icon: getSimpleIcon('CUSTOM', 'black'),
   },
 ];
